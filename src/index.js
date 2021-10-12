4566a6c9f696e7cdefc2ed7704849caa4ccc2a93
tomw66 <tom.wilson96@hotmail.com>
2021-10-12 20:49:46 +0100

    added API call and data processing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
-function component() {
-    const element = document.createElement('div');
+class Profile {
+    constructor(location, forecast, temperature, feelsLike, humidity, wind) {
+    this.location = location;
+    this.forecast = forecast;
+    this.temperature = temperature;
+    this.feelsLike = feelsLike;
+    this.humidity = humidity;
+    this.wind = wind;
+}}
 
-    element.innerHTML = "Hello!"
- 
-    return element;
-  }
- 
-  document.body.appendChild(component());
+async function getData(location, units) {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=c176b6076859914afc05d81f79ba3b12`, {mode: 'cors'});
+    const data = await response.json(); //handle errors somewhere, try/catch?
+    return data;
+};
+
+function createProfile(data) {
+    let cityProfile = new Profile(data.name, data.weather[0].main, data.main.temp, data.main.feels_like, data.main.humidity, data.wind.speed);
+    return cityProfile;
+}
+
+async function formSubmission(event) {
+    event.preventDefault();
+    const city = document.querySelector('#city');
+    const unitList = document.querySelectorAll('.radio');
+    let unit;
+    for (i = 0; i < unitList.length; i++) {
+        if(unitList[i].checked) {
+            unit = unitList[i];
+        }
+    }
+    let a = await getData(city.value, unit.value);
+    console.log(a)
+    let b = createProfile(a);
+    console.log(b);
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', formSubmission);
+//getData('london', 'metric');
