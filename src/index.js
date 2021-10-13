7b8ba1dc43c42a3f5e5cc8646f53dca33f30e469
tomw66 <tom.wilson96@hotmail.com>
2021-10-13 14:26:14 +0100

    basic working app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
+import './style.css';
+
 class Profile {
     constructor(location, forecast, temperature, feelsLike, humidity, wind) {
     this.location = location;
     this.forecast = forecast;
-    this.temperature = temperature;
-    this.feelsLike = feelsLike;
+    this.temperature = Math.round(temperature);
+    this.feelsLike = Math.round(feelsLike);
     this.humidity = humidity;
     this.wind = wind;
 }}
 
 async function getData(location, units) {
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=c176b6076859914afc05d81f79ba3b12`, {mode: 'cors'});
-    const data = await response.json(); //handle errors somewhere, try/catch?
+    const data = await response.json();
     return data;
 };
 
@@ -19,22 +21,37 @@ function createProfile(data) {
     return cityProfile;
 }
 
-function displayProfile(weather) {
-    //can we loop through these instead?
-    const card = document.querySelector('#card');
-    const locationDisplay = card.querySelector('#location');
-    const forecastDisplay = card.querySelector('#forecast');
-    const temperatureDisplay = card.querySelector('#temperature');
-    const feelsLikeDisplay = card.querySelector('#feelsLike');
-    const humidityDisplay = card.querySelector('#humidity');
-    const windDisplay = card.querySelector('#wind');
-
-    locationDisplay.innerText = weather.location;
-    forecastDisplay.innerText = weather.forecast;
-    temperatureDisplay.innerText = weather.temperature; //include unit and round number
-    feelsLikeDisplay.innerText = 'Feels like: ' + weather.feelsLike;
-    humidityDisplay.innerText = 'Humidity: ' + weather.humidity +'%';
-    windDisplay.innerText = 'Wind: ' + weather.wind; //also metric or imperial?
+function displayProfile(weather, units) {
+    let container = document.querySelector('#container');
+    let card = document.createElement('div');
+    card.classList.add("card");
+    container.appendChild(card);
+    let elements = ['location', 'forecast', 'temperature', 'feelsLike', 'humidity', 'wind']
+    elements.forEach(function(item) {
+        let a = 
+        item === 'location' ? 'h1' 
+        : item === 'forecast' ? 'h2' 
+        : 'p';
+        let p = document.createElement(a)
+        p.id = item + 'Display';
+        card.appendChild(p)
+    })
+    let tempUnit;
+    let windUnit;
+    if(units === 'metric') {
+        tempUnit = ' °C';
+        windUnit = ' metres/second';
+    }
+    else {
+        tempUnit = ' °F';
+        windUnit = ' miles/hour';
+    };
+    document.querySelector('#locationDisplay').innerText = weather.location;
+    document.querySelector('#forecastDisplay').innerText = weather.forecast;
+    document.querySelector('#temperatureDisplay').innerText = weather.temperature + tempUnit;
+    document.querySelector('#feelsLikeDisplay').innerText = 'Feels like: ' + weather.feelsLike + tempUnit;
+    document.querySelector('#humidityDisplay').innerText = 'Humidity: ' + weather.humidity +'%';
+    document.querySelector('#windDisplay').innerText = 'Wind: ' + weather.wind + windUnit;
 
 }
 
@@ -42,7 +59,7 @@ function formProcessing() {
     const city = document.querySelector('#city');
     const unitList = document.querySelectorAll('.radio');
     let unit;
-    for (i = 0; i < unitList.length; i++) {
+    for (let i = 0; i < unitList.length; i++) {
         if(unitList[i].checked) {
             unit = unitList[i];
         }
@@ -54,10 +71,18 @@ function formProcessing() {
 
 async function formSubmission(event) {
     event.preventDefault();
+    const errorLog = document.querySelector('#errorMessage');
     let params = formProcessing();
-    let rawData = await getData(params.location, params.unitChoice);
-    let processedData = createProfile(rawData);
-    displayProfile(processedData);
+    try {
+        errorLog.innerText = '';
+        let rawData = await getData(params.location, params.unitChoice);
+        let processedData = createProfile(rawData);
+        displayProfile(processedData, params.unitChoice);
+    }
+    catch(err) {
+        console.log(err)
+        errorLog.innerText = 'City not found! Try again.'
+    }
 }
 
 const form = document.querySelector('form');
