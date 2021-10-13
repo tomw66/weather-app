3ab114bb94100ea46c72db7d0867cc369ae155f1
tomw66 <tom.wilson96@hotmail.com>
2021-10-13 11:06:01 +0100

    working weather display

--- src/index.js
+++ src/index.js
@@ -19,8 +19,26 @@ function createProfile(data) {
     return cityProfile;
 }
 
-async function formSubmission(event) {
-    event.preventDefault();
+function displayProfile(weather) {
+    //can we loop through these instead?
+    const card = document.querySelector('#card');
+    const locationDisplay = card.querySelector('#location');
+    const forecastDisplay = card.querySelector('#forecast');
+    const temperatureDisplay = card.querySelector('#temperature');
+    const feelsLikeDisplay = card.querySelector('#feelsLike');
+    const humidityDisplay = card.querySelector('#humidity');
+    const windDisplay = card.querySelector('#wind');
+
+    locationDisplay.innerText = weather.location;
+    forecastDisplay.innerText = weather.forecast;
+    temperatureDisplay.innerText = weather.temperature; //include unit and round number
+    feelsLikeDisplay.innerText = 'Feels like: ' + weather.feelsLike;
+    humidityDisplay.innerText = 'Humidity: ' + weather.humidity +'%';
+    windDisplay.innerText = 'Wind: ' + weather.wind; //also metric or imperial?
+
+}
+
+function formProcessing() {
     const city = document.querySelector('#city');
     const unitList = document.querySelectorAll('.radio');
     let unit;
@@ -29,12 +47,18 @@ async function formSubmission(event) {
             unit = unitList[i];
         }
     }
-    let a = await getData(city.value, unit.value);
-    console.log(a)
-    let b = createProfile(a);
-    console.log(b);
+    let location = city.value;
+    let unitChoice = unit.value;
+    return {location, unitChoice}
+}
+
+async function formSubmission(event) {
+    event.preventDefault();
+    let params = formProcessing();
+    let rawData = await getData(params.location, params.unitChoice);
+    let processedData = createProfile(rawData);
+    displayProfile(processedData);
 }
 
 const form = document.querySelector('form');
 form.addEventListener('submit', formSubmission);
-//getData('london', 'metric');
